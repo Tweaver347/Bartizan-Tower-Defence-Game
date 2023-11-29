0766f691e8db8fe443069f40e8eea0bd3dd50803
Neel Dadhania <123699536+NeelKV@users.noreply.github.com>
2023-11-29 17:03:34 -0500

    Added code for A* in levelGridManager and made changes to Tile script

--- Bartizan/Assets/Tile.cs
+++ Bartizan/Assets/Tile.cs
@@ -8,11 +8,13 @@ public class Tile : MonoBehaviour
 
     [Header("Tile Attributes")]
     [SerializeField] private int xLoc, yLoc;
-    [SerializeField] private bool isPathable;
+    [SerializeField] private bool isPathable = true;
     [SerializeField] private GameObject tower;
 
     [Header("A* Attributes")]
-    [SerializeField] private int gCost, hCost;
+    [SerializeField] private int distTo, distFrom;
+    private Tile[] neighbours;
+    private Tile previous;
 
     [Header("References")]
     [SerializeField] private Color baseColor, offsetColor;
@@ -33,6 +35,24 @@ public class Tile : MonoBehaviour
         this.tower = tower;
         isPathable = false;
     }
+
+    public void setPrevious(Tile previous) { this.previous = previous; }
+
+    public Tile getPrevious() { return previous; }
+
+    public void setNeighbours(Tile[] neighbours) { this.neighbours = neighbours; }
+
+    public Tile[] getNeighbours() {  return neighbours; }
+
+    public void setDistTo(int dist) { distTo = dist; }
+
+    public int getDistTo() { return distTo;}
+
+    public void setDistFrom(int dist) { distFrom = dist; }
+
+    public int getDistFrom() {  return distFrom;}
+
+    public bool isEmpty() { return isPathable; }
     public void getTilePosition()
     {
         Debug.Log("Tile position is: (" + xLoc + ", " + yLoc + ")");
